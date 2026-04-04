f9edc28f4d3d074740b40fb2e2d78f39a2a9990b
rohansvibe <sunrohan34@gmail.com>
2026-04-04 21:28:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'fuel-calc-v2';
+const ASSETS = ['/index.html'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request).then(r => r || caches.match('/index.html')))
+  );
+});
